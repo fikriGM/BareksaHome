bb3440c51d4170616d8e1ff74aac3684b5443b77
fikriGM <>
2022-07-29 10:34:57 +0700

    first commit project

---
+++ TakeHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeHome
+//
+//  Created by Cashlez Macbook 2021 on 7/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
